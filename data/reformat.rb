756fab04c9b7168f354742e6d30e96e1a855bc0c
xun-gong <xun.gong@nyu.edu>
2015-04-13 23:18:28 -0400

    initial gh-pages repo

--- data/reformat.rb
+++
@@ -1,9 +0,0 @@
-require 'csv'
-
-data = CSV.read('daily_2014_clean.csv', headers: true)
-CSV.open('daily_2014_final.csv', 'w') do |csv|
-  csv << ['date','entity','avgASenti','avgImpSc']
-  data.each do |row|
-    csv << [row[0]+"-"+row[1]+"-"+row[2], row[3], row[4], row[5]]
-  end
-end
