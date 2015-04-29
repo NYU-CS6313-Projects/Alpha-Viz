1d79ee0b724de4f872f16628a56fbeb008c3f6fb
xun-gong <xun.gong@nyu.edu>
2015-04-28 23:54:06 -0400

    scope render + watch, auto refresh

--- js/strategyBar.js
+++ js/strategyBar.js
@@ -1,13 +1,8 @@
-// TO DO: 1. Need to solve data refresh problem (refreashed by click need to implement)
-//        2. Need to dynamic modify cretiria using slide
+// TO DO: Need to dynamic modify cretiria using slide
 angular.module('alphaViz')
 .directive('strategyBar', function() {
   function link(scope, element, attr) {
     // D3.js code
-    console.log(scope)
-    console.log(scope.data)  // ???? why undefinced, but actually show in scope.data? need parse?
-    // critieria of strategy
-    // TO DO: slide to dynamic modify them
     var sentiBar = 0.1
     var impactBar = 0
 
@@ -21,41 +16,49 @@ angular.module('alphaViz')
       .append('g')
         .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-    // draw
-    scope.$watch('data', function(data, old) {
+    var x = d3.time.scale().range([0, width - margin.left - margin.right])
+    var y = d3.scale.ordinal().range([height - margin.top - margin.bottom, 0]) 
 
-      // scale
-      var start = function(data){ return data[0].date }
-      var end = function(data){ return data[data.length - 1].date }
-      var x = d3.time.scale()
-          .domain([new Date(start(data)), d3.time.day.offset(new Date(end(data)), 1)])
-          .range([0, width - margin.left - margin.right])
-      var y = d3.scale.ordinal()
-          .domain(["sell","buy"])
-          .range([height - margin.top - margin.bottom, 0])  
-      var xAxis = d3.svg.axis()
+    var xAxis = d3.svg.axis()
           .scale(x)
           .orient('bottom')
           .ticks(d3.time.days, 31)
           .tickFormat(d3.time.format('%x'))
           .tickSize(3)
           .tickPadding(8)
-      var yAxis = d3.svg.axis()
+    var yAxis = d3.svg.axis()
           .scale(y)
           .orient('left')
           .tickPadding(8)
 
-      var bars = svg.selectAll('.bar_data')
-
-      if (data !== old) {
-        // bar
-        // .data(data).exit()
-        //   .remove()
-          console.log("changed")
-          console.log(data)
-          bars.data(data).enter()
+    // render based on data
+    scope.render = function(data) {
+      // set domains based on data
+      console.log('haha   ---    redering' + data[0].entity)
+      var start = function(data){ return data[0].date }
+      var end = function(data){ return data[data.length - 1].date }
+      x.domain([new Date(start(data)), d3.time.day.offset(new Date(end(data)), 1)])
+      y.domain(["sell","buy"])
+      // redraw axis
+      svg.selectAll('g.xaxis').remove()
+      svg.selectAll('g.yaxis').remove()
+      svg.selectAll('rect').remove()
+      // X, Y
+      svg.append('g').attr('class', 'xaxis')
+        .attr('transform', 'translate(0, ' + (height - margin.top - margin.bottom) + ')')
+        .call(xAxis)
+      svg.append('g')
+        .attr('class', 'yaxis')
+        .call(yAxis)
+      // X Axis text style
+      svg.selectAll(".xaxis text")  // select all the text elements for the xaxis
+        .attr("transform", function(d) {
+           return "translate(" + this.getBBox().height*-2 + "," + this.getBBox().height*+1.5 + ")rotate(-45)";
+       })
+      // Draw bar
+      var bars = svg.selectAll('rect')
+          .data(data).enter()
           .append('rect')
-          .attr("class", "bar_data")
           .attr('x', function(data) { return x(new Date(data.date)) })
           .attr('y', function(data){
             if (data.avgASenti >= sentiBar && data.avgImpSc >= impactBar)
@@ -77,23 +80,11 @@ angular.module('alphaViz')
           .on('mouseout', function(){
             d3.select(this).style('fill', 'steelblue')
           })
+    }
 
-          //bars.exit().remove();
-      }
-
-
-      svg.append('g').attr('class', 'xaxis')
-        .attr('transform', 'translate(0, ' + (height - margin.top - margin.bottom) + ')')
-        .call(xAxis)
-        svg.append('g')
-        .attr('class', 'yaxis')
-        .call(yAxis)
-      
-      // rotate xtext
-      svg.selectAll(".xaxis text")  // select all the text elements for the xaxis
-        .attr("transform", function(d) {
-           return "translate(" + this.getBBox().height*-2 + "," + this.getBBox().height*+1.5 + ")rotate(-45)";
-       });
+    // Watch 'data' and run scope.render(newVal)
+    scope.$watch('data', function() {
+      scope.render(scope.data)
     }, true)
   }
   return {
